442a3c3d044f12cb895861c161352205402694c2
Aboulma-ali Abdoulaye <aboulmaali.abdoulaye@gmail.com>
2026-01-29 14:04:45 +0000

    Add AWS debug + terraform outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,1 +1,11 @@
-#Hello
+output "instance_id" {
+	value = aws_instance.ec2_demo.id
+}
+
+output "public_ip" {
+	value = aws_instance.ec2_demo.public_ip
+}
+
+output "public_dns" {
+	value = aws_instance.ec2_demo.public_dns
+}
